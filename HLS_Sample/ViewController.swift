fa972bbf1451ea82a290af2268da4cf051f9a8e5
Nick Yang <nick10811@gmail.com>
2021-05-15 13:47:00 +0800

    Setup the play button with AutoLayout.

--- HLS_Sample/ViewController.swift
+++ HLS_Sample/ViewController.swift
@@ -11,9 +11,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // setup button
+        let playButton = UIButton(type: .system)
+        playButton.frame = CGRect(x: 50, y: 50, width: 100, height: 30)
+        playButton.setTitle("Play", for: .normal)
+        playButton.addTarget(self, action: #selector(clickButton(_:)), for: .touchUpInside)
+        self.view.addSubview(playButton)
+        
+        // setup constraints HINT: AutoLayout should be after addSubview()
+        playButton.translatesAutoresizingMaskIntoConstraints = false // AutoResize default is true for programmatically component
+        let centerX = NSLayoutConstraint(item: playButton, attribute: .centerX,
+                           relatedBy: .equal,
+                           toItem: self.view, attribute: .centerX,
+                           multiplier: 1, constant: 0)
+        let centerY = NSLayoutConstraint(item: playButton, attribute: .centerY,
+                           relatedBy: .equal,
+                           toItem: self.view, attribute: .centerY,
+                           multiplier: 1, constant: 0)
+        NSLayoutConstraint.activate([centerX, centerY])
+        
     }
 
+    @objc func clickButton(_ sender: UIButton) {
+        // TODO: play HLS video
+    }
 
 }
 
