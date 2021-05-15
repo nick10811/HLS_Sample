f720b417d0f20f5872ba38db2e88003d24ad7951
Nick Yang <nick10811@gmail.com>
2021-05-15 13:02:34 +0800

    Initial Commit

---
+++ HLS_Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HLS_Sample
+//
+//  Created by Nick Yang on 2021/5/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
